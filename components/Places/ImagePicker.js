ac6e22c4efad34d9aebf99818df8e87d3580aa12
ohwonsuk <wsoh51@gmail.com>
2024-05-04 20:30:39 +0900

    커스텀 버튼 생성하기

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,4 +1,4 @@
-import { Alert, Button, View, Image, Text, StyleSheet } from "react-native";
+import { Alert, View, Image, Text, StyleSheet } from "react-native";
 import {
   launchCameraAsync,
   useCameraPermissions,
@@ -7,6 +7,7 @@ import {
 import { useState } from "react";
 
 import { Colors } from "../../constants/colors";
+import OutlinedButton from "../UI/OutlinedButton";
 
 function ImagePicker() {
   const [pickedImage, setPickedImage] = useState();
@@ -58,7 +59,9 @@ function ImagePicker() {
   return (
     <View>
       <View style={styles.imagePreview}>{imagePreview}</View>
-      <Button title="Take Image" onPress={takeImageHandler} />
+      <OutlinedButton icon="camera" onPress={takeImageHandler}>
+        Take Image
+      </OutlinedButton>
     </View>
   );
 }
