84934eb5842549d696cdeb5318805eeb41dfcd8b
ohwonsuk <wsoh51@gmail.com>
2024-05-06 23:23:35 +0900

    지도에 마커 추가하기

--- screens/Map.js
+++ screens/Map.js
@@ -1,7 +1,10 @@
 import MapView, { Marker } from "react-native-maps";
 import { StyleSheet } from "react-native";
+import { useState } from "react";
 
 function Map() {
+  const [selectLocation, setSelectLocation] = useState();
+
   const region = {
     latitude: 37.78,
     longitude: -122.43,
@@ -9,7 +12,31 @@ function Map() {
     longitudeDelta: 0.0421,
   };
 
-  return <MapView style={styles.map} initialRegion={region}></MapView>;
+  function selectLocationHandler(event) {
+    console.log(event);
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+
+    setSelectLocation({ lat: lat, lng: lng });
+  }
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={region}
+      onPress={selectLocationHandler}
+    >
+      {selectLocation && (
+        <Marker
+          title="Picked Location"
+          coordinate={{
+            latitude: selectLocation.lat,
+            longitude: selectLocation.lng,
+          }}
+        />
+      )}
+    </MapView>
+  );
 }
 
 export default Map;
